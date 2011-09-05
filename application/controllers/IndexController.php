24d14d87fccd4daacbe779e4d739c2034a165bf9
Jace Ju <jaceju@gmail.com>
2011-09-05 22:58:24 +0800

    完成基本架構

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -13,6 +13,24 @@ class IndexController extends Zend_Controller_Action
 
     }
 
+    public function sessionsAction()
+    {
 
+    }
+
+    public function locationAction()
+    {
+
+    }
+
+    public function staffAction()
+    {
+
+    }
+
+    public function sponsorsAction()
+    {
+        
+    }
 }
 
