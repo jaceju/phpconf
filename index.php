81162c9e6445e91bf60ecec8586d1b3ba3641d48
Jace Ju <jaceju@gmail.com>
2011-10-15 11:39:42 +0800

    加入時區設定

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set("Asia/Taipei");
+
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/application'));
